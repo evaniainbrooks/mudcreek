3128e337ed19f759284b3be6f5c6b4fcc7e27601
Evan Brooks <evaniainbrooks@gmail.com>
2026-02-20 00:07:54 -0400

    bootstrapify

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  FLASH_CLASS_MAP = {
+    "notice"  => "success",
+    "alert"   => "danger",
+    "warning" => "warning",
+    "info"    => "info"
+  }.freeze
+
+  def bootstrap_flash_class(type)
+    FLASH_CLASS_MAP.fetch(type.to_s, "secondary")
+  end
 end
