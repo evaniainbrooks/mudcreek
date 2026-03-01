48e80b4074b99e6bacd33f23966b5043bd18ab3e
Evan Brooks <evaniainbrooks@gmail.com>
2026-03-01 10:44:18 -0400

    okg

--- db/migrate/20260228055717_add_name_to_tenant.rb
+++ db/migrate/20260228055717_add_name_to_tenant.rb
@@ -1,8 +1,12 @@
 class AddNameToTenant < ActiveRecord::Migration[8.1]
+  disable_ddl_transaction!
+
   def change
-    add_column :tenants, :name, :string
-    Tenant.reset_column_information
-    Tenant.find_each { |t| t.update_columns(name: "Default") }
-    change_column_null :tenants, :name, false
+    safety_assured do
+      add_column :tenants, :name, :string
+      Tenant.reset_column_information
+      Tenant.find_each { |t| t.update_columns(name: "Default") }
+      change_column_null :tenants, :name, false
+    end
   end
 end
