008c960b2578f4b4ab6f9ebff018596d2081966b
Evan Brooks <evaniainbrooks@gmail.com>
2026-03-01 10:36:57 -0400

    nuke

--- db/migrate/20260228055717_add_name_to_tenant.rb
+++ db/migrate/20260228055717_add_name_to_tenant.rb
@@ -1,5 +1,8 @@
 class AddNameToTenant < ActiveRecord::Migration[8.1]
   def change
-    add_column :tenants, :name, :string, null: false, default: nil
+    add_column :tenants, :name, :string
+    Tenant.reset_column_information
+    Tenant.find_each { |t| t.update_columns(name: "Default") }
+    change_column_null :tenants, :name, false
   end
 end
