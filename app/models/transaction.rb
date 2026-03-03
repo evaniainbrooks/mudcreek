42eaf2fcd646757e457b10420889ada17567ff3c
Evan Brooks <evaniainbrooks@gmail.com>
2026-03-03 02:28:43 -0400

    fix

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,8 +6,13 @@ class Transaction < ApplicationRecord
   native_enum :state, %i[pending succeeded failed]
 
   validates :order_id, uniqueness: { conditions: -> { where(state: "succeeded") } }
+  validates :uuid, presence: true
+  validates :uuid, uniqueness: true
+  validates :amount_cents, presence: true
 
   before_validation :ensure_uuid, on: :create
+
+  monetize :amount_cents
 
   private
 
