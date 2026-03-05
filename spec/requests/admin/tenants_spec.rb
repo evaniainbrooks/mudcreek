42c0b1ed530b1dac709b1ce5711ddd7e2c431967
Evan Brooks <evaniainbrooks@gmail.com>
2026-03-05 18:22:07 -0400

    tenantspec

---
+++ spec/requests/admin/tenants_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.describe "Admin::Tenants", type: :request do
+  before do
+    host! "example.com"
+    Current.tenant = Tenant.create!(key: "test", name: "Test", default: true)
+  end
+
+  let(:role) do
+    Role.create!(name: "tenant_manager", description: "Manage tenant").tap do |r|
+      r.permissions.create!(resource: "Tenant", action: "show")
+      r.permissions.create!(resource: "Tenant", action: "update")
+    end
+  end
+
+  let(:viewer) { create(:user, role: role) }
+  let!(:target) { create(:user) }
+
+  before { post session_path, params: { email_address: viewer.email_address, password: "password" } }
+
+  describe "GET /admin/tenant" do
+    it "returns 200", :aggregate_failures do
+      get admin_tenant_path
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include(Current.tenant.name)
+    end
+  end
+
+  describe "PATCH /admin/tenant" do
+    let(:params) { { tenant: { name: "New Name" } } }
+
+    it "returns 200", :aggregate_failures do
+      patch(admin_tenant_path, params:)
+
+      expect(response).to have_http_status(:found)
+
+      get admin_tenant_path
+
+      expect(response.body).to include("New Name")
+    end
+  end
+end
