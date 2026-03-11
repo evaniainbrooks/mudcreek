d63792e9575e84f7acc6a0e48a04a19460779588
Evan Brooks <evaniainbrooks@gmail.com>
2026-03-11 00:15:37 -0300

    refactor

--- app/components/footer_component.rb
+++ app/components/footer_component.rb
@@ -17,18 +17,18 @@ class FooterComponent < ViewComponent::Base
 
   def social_media_url(account)
     base = case account.platform
-           when "facebook"  then "https://facebook.com/"
-           when "instagram" then "https://instagram.com/"
-           when "youtube"   then "https://youtube.com/@"
-           when "twitter"   then "https://x.com/"
-           when "tiktok"    then "https://tiktok.com/@"
-           when "snapchat"  then "https://snapchat.com/add/"
-           when "linkedin"  then "https://linkedin.com/in/"
-           when "discord"   then "https://discord.gg/"
-           when "patreon"   then "https://patreon.com/"
-           when "onlyfans"  then "https://onlyfans.com/"
-           when "twitch"    then "https://twitch.tv/"
-           end
+    when "facebook"  then "https://facebook.com/"
+    when "instagram" then "https://instagram.com/"
+    when "youtube"   then "https://youtube.com/@"
+    when "twitter"   then "https://x.com/"
+    when "tiktok"    then "https://tiktok.com/@"
+    when "snapchat"  then "https://snapchat.com/add/"
+    when "linkedin"  then "https://linkedin.com/in/"
+    when "discord"   then "https://discord.gg/"
+    when "patreon"   then "https://patreon.com/"
+    when "onlyfans"  then "https://onlyfans.com/"
+    when "twitch"    then "https://twitch.tv/"
+    end
     "#{base}#{account.slug}"
   end
 end
