ab3a4ecaf41e30ab4356095e1cf1df9bfd1e4a54
Evan Brooks <evaniainbrooks@gmail.com>
2026-03-13 14:19:41 -0300

    refactor

---
+++ app/helpers/admin_helper.rb
@@ -0,0 +1,22 @@
+module AdminHelper
+  def admin_catalog_nav?
+    policy(Listing).index? ||
+      policy(Listings::Category).index? ||
+      policy(Listings::PropertySet).index? ||
+      policy(Lot).index? ||
+      policy(Auction).index? ||
+      policy(AuctionRegistration).index?
+  end
+
+  def admin_sales_nav?
+    policy(Order).index? ||
+      policy(Invoice).index? ||
+      policy(Offer).index? ||
+      policy(DiscountCode).index? ||
+      policy(DeliveryMethod).index?
+  end
+
+  def admin_settings_nav?
+    policy(User).index? || policy(Role).index? || policy(Tenant).index?
+  end
+end
