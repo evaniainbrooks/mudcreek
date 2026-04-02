8a2075deea4a63cf294e4315ebda6648df9cea12
Evan Brooks <evaniainbrooks@gmail.com>
2026-04-02 13:08:21 -0300

    fix

--- db/migrate/20260402110418_add_quantity_non_negative_constraint_to_listings.rb
+++ db/migrate/20260402110418_add_quantity_non_negative_constraint_to_listings.rb
@@ -3,7 +3,7 @@ class AddQuantityNonNegativeConstraintToListings < ActiveRecord::Migration[8.1]
 
   def change
     add_check_constraint :listings, "quantity IS NULL OR quantity >= 0",
-      name: "listings_quantity_non_negative", validate: false
+      name: "listings_quantity_non_negative", validate: false, if_not_exists: true
     validate_check_constraint :listings, name: "listings_quantity_non_negative"
   end
 end
