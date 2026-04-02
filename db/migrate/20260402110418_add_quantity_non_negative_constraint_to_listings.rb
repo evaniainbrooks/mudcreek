7d734a0e14e9fe47a2324e8a73619936e0600edd
Evan Brooks <evaniainbrooks@gmail.com>
2026-04-02 13:32:22 -0300

    fix

--- db/migrate/20260402110418_add_quantity_non_negative_constraint_to_listings.rb
+++ db/migrate/20260402110418_add_quantity_non_negative_constraint_to_listings.rb
@@ -1,9 +1,21 @@
 class AddQuantityNonNegativeConstraintToListings < ActiveRecord::Migration[8.1]
   disable_ddl_transaction!
 
-  def change
-    add_check_constraint :listings, "quantity IS NULL OR quantity >= 0",
-      name: "listings_quantity_non_negative", validate: false, if_not_exists: true
-    validate_check_constraint :listings, name: "listings_quantity_non_negative"
+  def up
+    constraint_exists = connection.check_constraints(:listings)
+                                  .any? { |c| c.name == "listings_quantity_non_negative" }
+
+    unless constraint_exists
+      safety_assured do
+        add_check_constraint :listings, "quantity IS NULL OR quantity >= 0",
+          name: "listings_quantity_non_negative", validate: false
+      end
+    end
+
+    safety_assured { validate_check_constraint :listings, name: "listings_quantity_non_negative" }
+  end
+
+  def down
+    remove_check_constraint :listings, name: "listings_quantity_non_negative"
   end
 end
