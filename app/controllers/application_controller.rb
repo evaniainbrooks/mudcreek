acc55ecc329a2418542eb996d1060e01254c6b47
Evan Brooks <evaniainbrooks@gmail.com>
2026-04-08 03:15:18 -0300

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -64,7 +64,7 @@ class ApplicationController < ActionController::Base
   def set_default_meta_tags
     set_meta_tags(
       site: Current.tenant.name,
-      og: { site_name: Current.tenant.name, type: "website" },
+      og: { site_name: Current.tenant.name, type: "website", url: request.original_url },
       twitter: { card: "summary" }
     )
   end
