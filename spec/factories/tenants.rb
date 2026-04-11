98415a61d24ac9e743b98d4ec32e1703730c4c66
Evan Brooks <evaniainbrooks@gmail.com>
2026-04-11 15:14:33 -0300

    spec

--- spec/factories/tenants.rb
+++ spec/factories/tenants.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :tenant do
     sequence(:key) { |n| "tenant_#{n}" }
     sequence(:name) { |n| "Tenant #{n}" }
+    email_address { "info@#{key}.example.com" }
     default { false }
   end
 end
