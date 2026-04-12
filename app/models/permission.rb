ddcc821d79d628d48241914a3488970b490625a5
Evan Brooks <evaniainbrooks@gmail.com>
2026-04-11 22:06:17 -0300

    validate

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,24 +2,37 @@ class Permission < ApplicationRecord
   include MultiTenant
 
   RESOURCES = %w[
-    Listing Lot User
-    Role Permission Listings::Category
-    Offer Order DiscountCode
-    DeliveryMethod Listings::RentalRatePlan
-    Auction AuctionListing AuctionRegistration
-    Bid Tenant Invoice
-    Listings::PropertySet
-    Listings::Property
-    Listings::DeliveryMethodSet
-    Listings::Delivery
-    Settlement
-    Page
-    ListingInferenceBatch
-    QrCode
-    Location
-    NavbarItem
+    Auction
+    AuctionListing
+    AuctionRegistration
+    Bid
+    DeliveryMethod
+    DiscountCode
+    Inquiry
+    InquiryForm
+    Invoice
     Ledger
     Ledger::Entry
+    Listing
+    ListingInferenceBatch
+    Listings::Category
+    Listings::Delivery
+    Listings::DeliveryMethodSet
+    Listings::Property
+    Listings::PropertySet
+    Listings::RentalRatePlan
+    Location
+    Lot
+    NavbarItem
+    Offer
+    Order
+    Page
+    Permission
+    QrCode
+    Role
+    Settlement
+    Tenant
+    User
   ].freeze
 
   ACTIONS   = %w[index show create update destroy reorder pay].freeze
@@ -33,9 +46,19 @@ class Permission < ApplicationRecord
   validates :resource, presence: true, inclusion: { in: RESOURCES }
   validates :action, presence: true, inclusion: { in: ACTIONS }, uniqueness: { scope: %i[role_id resource] }
 
+  validate :action_defined_on_resource, if: -> { resource.present? && action.present? }
   validate :immutable, on: :update
 
   private
+
+  def action_defined_on_resource
+    policy_class = "#{resource}Policy".safe_constantize
+    return errors.add(:resource, "does not have a policy") unless policy_class
+
+    unless policy_class.method_defined?(:"#{action}?")
+      errors.add(:action, "#{action.inspect} is not defined for #{resource}")
+    end
+  end
 
   def immutable
     errors.add(:base, "Permissions cannot be modified after creation")
