5d2a87fe9f39dc23c8ddbca7157e51721b4e1ec8
Evan Brooks <evaniainbrooks@gmail.com>
2026-04-14 21:49:55 -0300

    rm

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,9 +6,8 @@ module UsersHelper
     table.with_value_column("Role") { it.role }
     table.with_value_column("Created At", sort_attr: :created_at) { it.created_at }
     table.with_column("", html_class: "text-end") do |u|
-      buttons = [ link_to("View", admin_user_path(u), class: "btn btn-sm btn-outline-primary") ]
       unless u.activated?
-        buttons << button_to(
+        button_to(
           "Resend Activation",
           resend_activation_admin_user_path(u),
           method: :post,
@@ -16,7 +15,6 @@ module UsersHelper
           data: { turbo_confirm: "Resend activation email to #{u.email_address}?" }
         )
       end
-      safe_join(buttons, " ")
     end
     render(table)
   end
