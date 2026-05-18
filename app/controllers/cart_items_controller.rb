c6dda6bce1fa9d3b62f20320f6c670922a941ff3
Evan Brooks <evaniainbrooks@gmail.com>
2026-05-18 06:35:47 -0300

    fixes

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -79,7 +79,10 @@ class CartItemsController < ApplicationController
       token = session[:guest_cart_token]
       token ? CartItem.where(guest_cart_token: token) : CartItem.none
     end
-    scope.includes(listing: { gallery: { photos_attachments: :blob } }).order(:created_at)
+    scope.includes(
+      variant: [ { gallery: { photos_attachments: :blob } }, { option_values: :option } ],
+      listing: { gallery: { photos_attachments: :blob } }
+    ).order(:created_at)
   end
 
   def cart_items_scope
